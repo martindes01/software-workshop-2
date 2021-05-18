f6a3d5d1a1acb2ff8a84c40858201fb61bc2eae5
Martin de Spirlet <martindes01@gmail.com>
2021-05-18 21:30:29 +0100

    Update ServerListener.java
    - Add handlers for successful sign-in, timeline and user responses.
    - Keep track of a sign-in response index that is passed to the client
      controller on successful sign-in to determine which username resulted
      in the successful sign-in.

--- assignment-2/client/src/main/java/com/bham/fsd/assignments/jabberclient/ServerListener.java
+++ assignment-2/client/src/main/java/com/bham/fsd/assignments/jabberclient/ServerListener.java
@@ -2,7 +2,6 @@ package com.bham.fsd.assignments.jabberclient;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.net.Socket;
 
 import com.bham.fsd.assignments.jabberserver.JabberMessage;
 
@@ -12,42 +11,66 @@ import javafx.application.Platform;
  * The server listener class implements the Java runnable interface. It listens
  * for incoming responses from the server and handles them by calling the
  * relevant methods of the Jabber client controller.
+ *
+ * @author Martin de Spirlet
  */
 public class ServerListener implements Runnable {
 
     private static final String SIGN_IN_RESPONSE_FAILURE = "unknown-user";
     private static final String SIGN_IN_RESPONSE_SUCCESS = "signedin";
 
-    public Socket socket;
-    public ObjectInputStream in;
+    private static final String TIMELINE_RESPONSE = "timeline";
+    private static final String USERS_RESPONSE = "users";
+
+    private final JabberClient client;
+    private final ObjectInputStream in;
+
+    private int usernameIndex = 0;
 
     /**
-     * Creates a new server listener for the specified socket and the specified
-     * object input stream that wraps the input stream of the socket.
+     * Creates a new server listener for the specified client and object input
+     * stream from the client to the server. The listener continues to run while the
+     * client is connected to the server, or until an I/O exception occurs.
      *
-     * @param socket the socket connected to the server
-     * @param in     the object input stream that wraps the input stream of the
-     *               socket
+     * @param client the client for which to create this server listener
+     * @param in     the object input stream from the client to the server
      */
-    public ServerListener(Socket socket, ObjectInputStream in) {
-        this.socket = socket;
+    public ServerListener(JabberClient client, ObjectInputStream in) {
+        this.client = client;
         this.in = in;
     }
 
     @Override
     public void run() {
         try {
-            while (!socket.isClosed()) {
+            while (client.isConnected()) {
                 JabberMessage response = (JabberMessage) in.readObject();
-                System.out.println(response.getMessage());
 
                 switch (response.getMessage()) {
                     case SIGN_IN_RESPONSE_FAILURE:
-                        Platform.runLater(() -> JabberDialogs.showSignInErrorDialog());
+                        usernameIndex++;
+                        if (!client.isSignedIn()) {
+                            Platform.runLater(() -> JabberDialogs.showSignInErrorDialog());
+                        }
                         break;
 
                     case SIGN_IN_RESPONSE_SUCCESS:
-                        // TODO
+                        if (!client.isSignedIn()) {
+                            client.setSignedInUsername(usernameIndex);
+                            Platform.runLater(() -> client.showDashboardView());
+                        }
+                        break;
+
+                    case TIMELINE_RESPONSE:
+                        if (client.isSignedIn()) {
+                            Platform.runLater(() -> client.handleTimelineResponse(response.getData()));
+                        }
+                        break;
+
+                    case USERS_RESPONSE:
+                        if (client.isSignedIn()) {
+                            Platform.runLater(() -> client.handleUsersResponse(response.getData()));
+                        }
                         break;
                 }
             }
