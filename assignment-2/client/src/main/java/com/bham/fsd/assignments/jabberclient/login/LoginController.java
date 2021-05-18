7b9036c59e390e4a6acdc3990915103849afe67e
Martin de Spirlet <martindes01@gmail.com>
2021-05-18 14:28:03 +0100

    Add JabberDialogs.java
    - Add class for displaying common dialogs.
    - Move the 'showSignInErrorDialog' method from 'LoginController.java' to
      'JabberDialogs.java'.

--- assignment-2/client/src/main/java/com/bham/fsd/assignments/jabberclient/login/LoginController.java
+++ assignment-2/client/src/main/java/com/bham/fsd/assignments/jabberclient/login/LoginController.java
@@ -7,10 +7,8 @@ import com.bham.fsd.assignments.jabberclient.JabberClient;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
@@ -24,10 +22,6 @@ public class LoginController {
 
     private static final String VIEW_TITLE = "Jabber Login";
     private static final String VIEW_FILENAME = "login.fxml";
-
-    private static final String SIGN_IN_ERROR_TITLE = "Sign-in error";
-    private static final String SIGN_IN_ERROR_HEADER_TEXT = "Username not found";
-    private static final String SIGN_IN_ERROR_CONTENT_TEXT = "The server was unable to find this username in the database.\nPlease try again.";
 
     private JabberClient client;
     private Parent view;
@@ -79,18 +73,6 @@ public class LoginController {
     }
 
     /**
-     * Displays a modal error dialog that informs the user that the provided
-     * username was not found by the server in the database.
-     */
-    public void showSignInErrorDialog() {
-        Alert alert = new Alert(AlertType.ERROR);
-        alert.setTitle(SIGN_IN_ERROR_TITLE);
-        alert.setHeaderText(SIGN_IN_ERROR_HEADER_TEXT);
-        alert.setContentText(SIGN_IN_ERROR_CONTENT_TEXT);
-        alert.showAndWait();
-    }
-
-    /**
      * Returns the title of the view associated with this controller.
      *
      * @return the title of the view associated with this controller
@@ -114,4 +96,5 @@ public class LoginController {
 
         return view;
     }
+
 }
